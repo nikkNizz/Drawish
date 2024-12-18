21af6cbe01f72428755d67e2fbda429141ebc6d6
nikkNizz <nicniz@libero.it>
2024-12-18 20:44:45 +0100

    Update curvelinearea.cpp

--- curvelinearea.cpp
+++ curvelinearea.cpp
@@ -16,7 +16,33 @@ curveLineArea::curveLineArea(QWidget *parent)  : QLabel{parent}
 
 void curveLineArea::mousePressEvent(QMouseEvent *event)
 {
+    if(sizes::activeOperation == 11){
+        if(sizes::shape_x_begin == -1 && sizes::shape_y_begin == -1){
+            sizes::shape_x_begin = event->pos().x();
+            sizes::shape_y_begin = event->pos().y();
+            QPainter p(&tPix);
+            QPen pen(sizes::activeColor);
+            pen.setWidth(sizes::line_width);
+            p.setPen(pen);
+            p.drawPoint(event->pos());
+            this->setPixmap(tPix);
+        }else{
+            sizes::shape_x_end = event->pos().x();
+            sizes::shape_y_end = event->pos().y();
+            QPainter p(&tPix);
+            QPen pen(sizes::activeColor);
+            pen.setWidth(sizes::line_width);
+            p.setPen(pen);
+            p.drawLine(sizes::shape_x_begin, sizes::shape_y_begin, sizes::shape_x_end, sizes::shape_y_end);
+            this->setPixmap(tPix);
+            sizes::shape_x_begin = sizes::shape_x_end;
+            sizes::shape_y_begin = sizes::shape_y_end;
+            p.end();
+            emit finishLines();
+        }
 
+    }
+    else if(sizes::activeOperation == 9){
     if(countClicks ==0){
         startPoint = event->pos();
         QPainter p(&tPix);
@@ -55,14 +81,14 @@ void curveLineArea::mousePressEvent(QMouseEvent *event)
         pen.setWidth(sizes::line_width);
         p.setPen(pen);
         p.drawPath(pp);
-        this->setPixmap(tPix);
-    }
-    else{
+        this->setPixmap(tPix);        
+    //}else{
+        p.end();
         countClicks=0;
         emit finishCurve();
         return;
-    }
+   }
     countClicks++;
+    }
 }
-
 
