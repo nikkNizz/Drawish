f393c52bf28854bbe3f1021aaa2be9e451bc118f
nikkNizz <nicniz@libero.it>
2025-01-23 20:50:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include "mainwindow.h"
+#include "geometric.h"
 
 #include <QApplication>
+#include <QTranslator>
+#include <QLocale>
+#include <QString>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QString local =  QLocale::languageToString(QLocale::system().language());
+
+    QTranslator trs;
+    trs.load(":/it_lang.qm");
+    if(local == QString("Italian")){
+        a.installTranslator(&trs);
+    }
+    if(argc > 1){
+        sizes::passedFile =argv[1];
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
