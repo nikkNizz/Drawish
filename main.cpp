e934bfc5d389aa56ea279ec3f2ead71108e3fd3d
nikkNizz <nicniz@libero.it>
2025-01-29 20:26:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,21 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    bool trsl = true;
+    if(argc == 2){
+        if(QString(argv[1]) == "en"){ trsl = false; }
+        else{ sizes::passedFile =argv[1];}
+    }else if(argc > 2){
+        sizes::passedFile =argv[1];
+        if(QString(argv[2]) == "en"){ trsl = false; }
+    }
+
     QString local =  QLocale::languageToString(QLocale::system().language());
 
     QTranslator trs;
     trs.load(":/it_lang.qm");
-    if(local == QString("Italian")){
+    if(local == QString("Italian") && trsl == true){
         a.installTranslator(&trs);
-    }
-    if(argc > 1){
-        sizes::passedFile =argv[1];
     }
 
     MainWindow w;
