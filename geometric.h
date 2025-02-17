55689dff24401d431f0560bbe6ca586fd15bd8aa
nikkNizz <nicniz@libero.it>
2025-02-17 20:32:50 +0100

    Update geometric.h

--- geometric.h
+++ geometric.h
@@ -5,16 +5,17 @@
 
 struct sizes
 {
- static QString passedFile;
- static int areaWidth;
+  static QString passedFile;
+  static int areaWidth;
   static int areaHeight;
   static bool startResize;
-  static int activeOperation; //1=selection  2=text 3=pen 4=fill 5=spray  6=picker 7=line 8=shape 9=curve 10= double pen 11 = connLine
+  static int activeOperation; //1=selection  2=text 3=pen 4=fill 5=spray  6=picker 7=line 8=shape 9=curve 10= conn_curve 11 = connLine 12=stretch
 // selection geometry
   static int selX;
   static int selY;
   static int selW;
   static int selH;
+  //----------------
   static bool isSelectionOn;
   static bool isShapeOn;
   static QColor activeColor;
@@ -26,13 +27,13 @@ struct sizes
   static int shape_y_end;
   static QString activeShape;
   static int FillStyle;
-  static int stopShow;
   static bool isCurveLineAreaOn;
   static int zoomx;
   static int zoomy;
   static bool zoomEdited;
   static int lineXEnd;
   static int lineYEnd;
+  static bool curveStretch;
 };
 
 #endif // GEOMETRIC_H
