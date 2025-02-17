2ad5d2eddde862d54064ce9e06def44a5ce6f5f7
nikkNizz <nicniz@libero.it>
2025-02-17 20:29:43 +0100

    Update curvelinearea.cpp

--- curvelinearea.cpp
+++ curvelinearea.cpp
@@ -16,7 +16,7 @@ curveLineArea::curveLineArea(QWidget *parent)  : QLabel{parent}
 
 void curveLineArea::mousePressEvent(QMouseEvent *event)
 {
-    if(sizes::activeOperation == 11){
+    if(sizes::activeOperation == 11){  // lines
         if(sizes::shape_x_begin == -1 && sizes::shape_y_begin == -1){
             sizes::shape_x_begin = event->pos().x();
             sizes::shape_y_begin = event->pos().y();
@@ -26,7 +26,7 @@ void curveLineArea::mousePressEvent(QMouseEvent *event)
             p.setPen(pen);
             p.drawPoint(event->pos());
             this->setPixmap(tPix);
-        }else{
+        }else{            
             sizes::shape_x_end = event->pos().x();
             sizes::shape_y_end = event->pos().y();
             QPainter p(&tPix);
@@ -42,15 +42,28 @@ void curveLineArea::mousePressEvent(QMouseEvent *event)
         }
 
     }
-    else if(sizes::activeOperation == 9){
+    else if(sizes::activeOperation == 9 || sizes::activeOperation == 10 ){  // curve
     if(countClicks ==0){
-        startPoint = event->pos();
+            if(sizes::activeOperation == 10 && sizes::shape_x_begin != -1 && sizes::shape_y_begin != -1 ){
+                startPoint = QPoint(sizes::shape_x_begin, sizes::shape_y_begin);
+                centerPoint1 = event->pos();
+                QPainter p(&tPix);
+                QPen pen(sizes::activeColor);
+                pen.setWidth(sizes::line_width);
+                p.setPen(pen);
+                p.drawPoint(centerPoint1);
+                this->setPixmap(tPix);
+                countClicks++;
+            }
+            else{     startPoint = event->pos();
         QPainter p(&tPix);
         QPen pen(sizes::activeColor);
         pen.setWidth(sizes::line_width);
         p.setPen(pen);
         p.drawPoint(startPoint);
         this->setPixmap(tPix);
+            }
+
     }
     else if(countClicks ==1){
         centerPoint1 = event->pos();
@@ -72,6 +85,8 @@ void curveLineArea::mousePressEvent(QMouseEvent *event)
     }
     else if(countClicks ==3){
         endPoint = event->pos();
+        sizes::shape_x_begin = endPoint.x();
+        sizes::shape_y_begin = endPoint.y();
         tPix.fill(QColor(255,255,255,0));
         QPainterPath pp;
         pp.moveTo(startPoint);
@@ -82,7 +97,6 @@ void curveLineArea::mousePressEvent(QMouseEvent *event)
         p.setPen(pen);
         p.drawPath(pp);
         this->setPixmap(tPix);        
-    //}else{
         p.end();
         countClicks=0;
         emit finishCurve();
