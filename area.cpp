059e8bdf3e525a104e7e57ae57cd58ad3ef00838
nikkNizz <nicniz@libero.it>
2025-02-17 20:26:44 +0100

    Update area.cpp

--- area.cpp
+++ area.cpp
@@ -42,7 +42,7 @@ void Area::mousePressEvent(QMouseEvent *event)
         sizes::selW = 160;
         emit endRubber();   // reuse this
     }
-    else if(sizes::activeOperation == 3 || sizes::activeOperation == 10){
+    else if(sizes::activeOperation == 3 ){
         sizes::selX = event->pos().x();
         sizes::selY = event->pos().y();
         sizes::shape_x_begin = sizes::selX;
@@ -93,13 +93,7 @@ void Area::mouseMoveEvent(QMouseEvent *event)
        sizes::shape_y_end = sizes::selY;
       emit penDraw();
     }
-   else if(sizes::activeOperation == 10){
-       sizes::selX = event->pos().x();
-       sizes::selY = event->pos().y();
-       sizes::shape_x_end = sizes::selX;
-       sizes::shape_y_end = sizes::selY;
-      emit doublePenDraw();
-    }
+
    else if(sizes::activeOperation == 5){
        sizes::selX = event->pos().x();
        sizes::selY = event->pos().y();
@@ -140,7 +134,7 @@ void Area::mouseReleaseEvent(QMouseEvent *event)
          delete rubb;
          emit endRubber();
      }
-   else if(sizes::activeOperation == 3 || sizes::activeOperation == 5 || sizes::activeOperation == 10 ){
+   else if(sizes::activeOperation == 3 || sizes::activeOperation == 5 ){
          if(!rightPressed) emit finishDrawPen();
      }
 }
