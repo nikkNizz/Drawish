3fab37622f14602c593e3dd15ad2110e90e3d261
nikkNizz <nicniz@libero.it>
2025-04-15 17:41:17 +0200

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -1,12 +1,14 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 #include <QString>
+#include <QColor>
 
 class fileIO
 {
 public:
     fileIO();
     void createFile(QString content, QString path);
+    void saveFav_color(QColor col, QString path);
     QString readFile(QString path);
 };
 
