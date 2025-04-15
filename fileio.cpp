ee91d9ca9560f0c7250159bb46dd40f01cbb49ee
nikkNizz <nicniz@libero.it>
2025-04-15 17:40:33 +0200

    Update fileio.cpp

--- fileio.cpp
+++ fileio.cpp
@@ -1,6 +1,11 @@
 #include "fileio.h"
 #include <QTextStream>
 #include <QFile>
+#include <QColor>
+#include <QDir>
+#include <qpainter.h>
+#include <qpixmap.h>
+#include<QFile>
 
 fileIO::fileIO() {}
 
@@ -13,6 +18,33 @@ void fileIO::createFile(QString content, QString path)
         ff.flush();
         ff.close();
 
+}
+
+void fileIO::saveFav_color(QColor col, QString path)
+{
+
+    QPixmap oneColor(40, 32);
+    oneColor.fill(col);
+
+    if(QFile::exists(path) == false){
+        oneColor.save(path);
+    }else{
+        QPixmap oldPalette(path);
+        int len = oldPalette.width();
+        if(len > 639){
+            len = 600;
+        }
+        oldPalette = oldPalette.copy(0, 0, len, 32);
+        QPixmap newPalette(len+40, 40);
+        newPalette.fill(Qt::white);
+        QPainter p(&newPalette);
+        p.drawPixmap(39,0, oldPalette);
+        p.end();
+        QPainter p2(&newPalette);
+        p2.drawPixmap(0,0, oneColor);
+        p2.end();
+        newPalette.save(path);
+    }
 }
 
 QString fileIO::readFile(QString path)
